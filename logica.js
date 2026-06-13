2fdcdb713c65a54f8a0c3d954ec86620dcd57d0c
Arthur_SemCódigo <arthurmiguellimabrito139@gmail.com>
2026-06-13 15:50:32 -0300

    projeto atualizado para rolar 2 dados e mostrar seu resultado

--- logica.js
+++ logica.js
@@ -1,13 +1,34 @@
-let img = document.querySelector('img');
+let img = document.querySelectorAll('img')[0];
+let img2 = document.querySelectorAll('img')[1];
 let botao = document.querySelector('.myButton');
 
-function jogaDado () { 
+function jogaDado() {
+    // Declara as variáveis fora para ambos acessarem
+    let num, num2;
 
-let num = Math.floor(Math.random() * 6) + 1;
-let ramdomDiceImg = 'images/' + 'dice' + num + '.png'; 
-img.setAttribute('src', ramdomDiceImg);
+    img.classList.add('dado-rolando');
+    img2.classList.add('dado-rolando');
 
+    img.addEventListener('animationend', () => {
+        num = Math.floor(Math.random() * 6) + 1;
+        img.setAttribute('src', 'images/dice' + num + '.png');
+        img.classList.remove('dado-rolando');
+
+        if (num2 !== undefined) {
+            document.getElementById("result").innerHTML = num + num2;
+        }
+    }, { once: true });
+
+    img2.addEventListener('animationend', () => {
+        num2 = Math.floor(Math.random() * 6) + 1;
+        img2.setAttribute('src', 'images/dice' + num2 + '.png');
+        img2.classList.remove('dado-rolando');
+
+        if (num !== undefined) {
+            document.getElementById("result").innerHTML = num + num2;
+        }
+    }, { once: true });
 }
 
 botao.addEventListener('click', jogaDado);
-jogaDado()
+jogaDado();
